dcfadac912dbc4fbd91c75696a5634910236fe7d
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-25 21:24:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function glav() {
+window.location.href="https://miroshkin0207/github.io/miroshkin/"
+}
+function klick() {
+let schet = document.getElementById("schet")
+let kolvo = document.getElementById("kolvo")
+kolvo.innerHTML += 1
+}
