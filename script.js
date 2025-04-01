08c9bc95f68c4f178093f1b1c0c7fd43ee42d5df
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-04-01 21:04:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function glav() {
 window.location.href="https://miroshkin0207.github.io/miroshkin/"
 }
 function klick() {
-let schet = document.getElementById("schet")
-let kolvo = Number(document.getElementById("kolvo"))
-kolvo.innerHTML += 1
+let kolvo = document.getElementById("kolvo")
+let count = kolvo.innerHTML
+count ++
+kolvo.innerHTML = count
 }
